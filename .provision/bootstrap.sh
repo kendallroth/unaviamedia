337ede80c2e67547f2b497eb477a382d75d983c7
UnaviaMedia <kroth@unaviamedia.ca>
2016-08-26 18:04:12 -0400

    Fix provision and vagrant files

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -3,14 +3,14 @@
 #sudo bash
 
 echo "Updating repositores"
-sudo apt update -y > /dev/null
+sudo apt-get update -y > /dev/null
 
 ##################################################
 # nginx
 echo "Nginx Setup"
 
 echo "Installing nginx"
-sudo apt install -y nginx > /dev/null
+sudo apt-get install -y nginx > /dev/null
 sudo service nginx start > /dev/null
 
 echo "Configuring nginx"
@@ -22,8 +22,7 @@ ln -s /etc/nginx/sites-available/site_conf /etc/nginx/sites-enabled/site_conf
 sudo rm -rf /etc/nginx/sites-available/default
 sudo rm -rf /etc/nginx/sites-enabled/default
 
-#sudo service nginx restart > /dev/null
-sudo systemctl reload nginx > /dev/null
+sudo service nginx restart > /dev/null
 
 # clean /var/www
 sudo rm -Rf /var/www
@@ -36,22 +35,18 @@ ln -s /vagrant /var/www
 echo "PHP Setup"
 
 echo "Updating php repositories"
-#sudo apt install -y python-software-properties build-essential > /dev/null
-#sudo add-apt-repository ppa:ondrej/php5 > /dev/null
-#sudo add-apt-repository ppa:ondrej/php > /dev/null
-sudo apt update > /dev/null
+sudo apt-get install -y python-software-properties build-essential > /dev/null
+sudo add-apt-repository ppa:ondrej/php5 > /dev/null
+sudo apt-get update > /dev/null
 
 echo "Installing php"
-#sudo apt install -y php5-common php5-dev php5-cli php5-fpm > /dev/null
-sudo apt install php-fpm php-mysql
+sudo apt-get install -y php5-common php5-dev php5-cli php5-fpm > /dev/null
 
 echo "Installing php extensions"
-#sudo apt install -y curl php5-curl php5-gd php5-mcrypt php5-mysql > /dev/null
-sudo apt install curl
+sudo apt-get install -y curl php5-curl php5-gd php5-mcrypt php5-mysql > /dev/null
 
-sudo sed -i s/\;cgi\.fix_pathinfo\s*\=\s*l/cgi.fix_pathinfo\=0/ /etc/php/7.0/fpm/php.ini
-#sudo service php7-fpm restart > /dev/null
-sudo systemctl restart php7.0-fpm > /dev/null
+sudo sed -i s/\;cgi\.fix_pathinfo\s*\=\s*l/cgi.fix_pathinfo\=0/ /etc/php5/fpm/php.ini
+sudo service php5-fpm restart > /dev/null
 sudo service nginx restart > /dev/null
 
 ##################################################
@@ -59,14 +54,14 @@ sudo service nginx restart > /dev/null
 #echo "MySQL Setup"
 
 #echo "Installing automation utilities"
-#sudo apt install -y debconf-utils > /dev/null
+#sudo apt-get install -y debconf-utils > /dev/null
 
 #echo "Setting mysql setup variables"
 #debconf-set-selections <<< 'mysql-server mysql-server/root_password password Passw0rd'
 #debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password Passw0rd'
 
 #echo "Installing mysql"
-#sudo apt install -y mysql-server > /dev/null
+#sudo apt-get install -y mysql-server > /dev/null
 
 
 ##################################################
@@ -74,19 +69,22 @@ sudo service nginx restart > /dev/null
 echo "Git Setup"
 
 echo "Installing git"
-sudo apt install -y git > /dev/null
+sudo apt-get install -y git > /dev/null
 
 ##################################################
 # nodejs
 echo "NodeJS Setup"
 
 echo "Updating NodeJS repositories"
-sudo curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash - > /dev/null
+sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - > /dev/null
 
 echo "Installing nodejs"
-sudo apt install -y nodejs > /dev/null
+sudo apt-get install -y nodejs > /dev/null
 
 ##################################################
 # foundation
 echo "Foundation Setup"
 sudo npm install --global foundation-cli > /dev/null
+
+echo "Node Packages Install"
+npm install
