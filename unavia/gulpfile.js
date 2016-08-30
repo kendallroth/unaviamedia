6c37a72a382b1e97bd712e6e8c1e16b265f71663
UnaviaMedia <kroth@unaviamedia.ca>
2016-08-30 16:57:35 -0400

    Fixed gulpfile

--- unavia/gulpfile.js
+++ unavia/gulpfile.js
@@ -1,24 +1,38 @@
-var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
+var gulp = require("gulp");
+var $    = require("gulp-load-plugins")();
 
-var sassPaths = [
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
-];
-
-gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'));
+gulp.task("sass", function() {
+	setTimeout(function() {
+		return gulp.src("src/scss/app.scss")
+			//Compile the SCSS to CSS
+			.pipe($.sass({
+				includePaths: [
+					"bower_components/foundation-sites/scss",
+					"bower_components/motion_ui/src"
+				]
+			}))
+			//Add vendor prefixes to CSS
+			.pipe($.autoprefixer({
+				browsers: ["last 2 version"],
+				cascade: false
+			}))
+			//Output the CSS
+			.pipe(gulp.dest("dist/css"));
+	}, 500);
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+gulp.task("scripts", function() {
+	return gulp.src(["src/app.js"])
+		//Output the JS
+		.pipe(gulp.dest("dist/js"));
 });
+
+
+//Gulp watch task
+gulp.task("watch", ["sass", "scripts"], function() {
+	gulp.watch("src/scss/*.scss", ["sass"]);
+	gulp.watch("src/js*.js", ["scripts"]);
+});
+
+//Default gulp task
+gulp.task("default", ["watch"]);
