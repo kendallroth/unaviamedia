c8da18bb92ad1090ac43a1b81b7697a7fb815b29
UnaviaMedia <kroth@unaviamedia.ca>
2016-09-15 23:59:51 -0400

    Add footer icons and text and styles

--- unavia/include/footer.php
+++ unavia/include/footer.php
@@ -2,8 +2,27 @@
 
 ?>
 
-<footer>
-
+<footer class="footer-bar">
+	<!-- Row of social media icons -->
+	<div class="row social-media-bar">
+		<div class="column text-center">
+			<a href="//twitter.com/UnaviaMedia">
+				<img src="//placehold.it/30x30" />
+			</a>
+			<a href="//github.com/UnaviaMedia">
+				<img src="//placehold.it/30x30" />
+			</a>
+			<a href="//ca.linkedin.com/in/kendallroth">
+				<img src="//placehold.it/30x30" />
+			</a>
+		</div>
+	</div>
+	<!-- Copyright notice -->
+	<div class="row align-center">
+		<div class="column text-center">
+			<span>&copy; UnaviaMedia 2016</span>
+		</div>
+	</div>
 </footer>
 
 </body>
