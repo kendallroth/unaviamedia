ee3bfd160efe8dcc28088809775e776369930b38
UnaviaMedia <kroth@unaviamedia.ca>
2016-10-01 10:05:50 -0400

    Update bootstrap script

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -2,26 +2,34 @@
 adduser development
 usermod -aG sudo development
 su development
+mkdir /var/www -p
 cd /var/www
 # Will need to enter password for first sudo use
 
-# Clone git repository and link web root and nginx server block
-git clone https://github.com/unaviamedia/unaviamedia.git
-ln -s /var/www/unaviamedia/unavia /var/www/html
-sudo ln -s /var/www/unaviamedia/.provision/nginx/site_conf /etc/nginx/sites-available/site_conf
-
 # Update repositories and upgrade available packages
+echo "Update repositories and upgrade available packages"
 sudo apt update -y
 sudo apt upgrade -y
 
+# Install git
+echo "Install git"
+sudo apt install git -y
+
+# Clone git repository and link web root and nginx server block
+echo "Clone repository and link web root and nginx server block"
+git clone https://github.com/unaviamedia/unaviamedia.git
+
 # Install nginx
+echo "Install nginx"
 sudo apt install nginx -y
 # Possible check to ensure nginx is working (go to ip)
 
 # Install mysql
+echo "Install mysql"
 sudo apt install mysql-server
 # Add password automatically ("Passw0rD")
 
+echo "Secure mysql installation"
 sudo mysql_secure_installation
 # Validate password (n)
 # Change password (n)
@@ -31,40 +39,48 @@ sudo mysql_secure_installation
 # Reload table privileges (y)
 
 # Install php and mysql handling
+echo "Install PHP and MySQL handling"
 sudo apt install php-fpm php-mysql -y
 
 # Security configuration to php.ini (/etc/php/7.0/fpm/php.ini) and restart php
 # Find/replace "#cgi.fix_pathinfo=1" to "cgi.fix_pathinfo=0"
+echo "Security fix for php.ini"
 sudo systemctl restart php7.0-fpm
 
 # Move default site configuration file into sites-available and link to sites-enabled
-
+echo "Setup nginx site configuration files"
 cd /etc/nginx/sites-enabled/
-sudo rm default
+sudo rm -f default
+sudo ln -s /var/www/unaviamedia/.provision/nginx/site_conf /etc/nginx/sites-available/site_conf
 sudo ln -s /etc/nginx/sites-available/site_conf /etc/nginx/sites-enabled/site_conf
-cd -
+cd /var/www
 
 # Check nginx configuration and reload
+echo "Check nginx configuration and reload"
 sudo nginx -t
 sudo systemctl reload nginx
 
 # Clean html directory and create php test file
-sudo rm /var/www/html/*
+echo "Clean web root and link repository web files"
+sudo ln -s /var/www/unaviamedia/unavia /var/www/html
 sudo vim /var/www/html/info.php
 
 # Update web directory owner and permissions (set 755 for directories and 644 for files)
 #   This will need to be run frequently (after changes) until permission inheritance is set
+echo "Update web directory permission"
 sudo chown -R "$USER":www-data /var/www
 chmod -R u+rwX,go+rX,go-w /var/www
 
 # Install nodejs and npm, and link nodejs to node (PATH issues)
-sudo apt install nodejs
-sudo apt install npm
+echo "Install nodejs and npm"
+sudo apt install nodejs npm -y
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 
 # Install gulp globally
-sudo npm install --global npm
+echo "Install gulp globally"
+sudo npm install --global gulp
 
 # Install node packages
+echo "Install node packages"
 cd /var/www/html
 npm install
