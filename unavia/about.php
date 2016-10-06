e08930a80d62f719ddeb959a6a3e30e277cfeeb2
UnaviaMedia <kroth@unaviamedia.ca>
2016-10-05 21:12:49 -0400

    Add links to About page subheaders and relating styles

--- unavia/about.php
+++ unavia/about.php
@@ -10,28 +10,31 @@ require_once(HEADER_FRAGMENT);
 			<h1>About Me</h1>
 		</div>
 	</div>
-	<div class="row about-introduction">
+	<div class="row about-subsection about-introduction">
 		<div class="column">
 			<h2>Introduction</h2>
 			<p>Hi, my name is Kendall Roth. I am currently pursuing my dream as a developer by attending Conestoga College for Computer Programming/Analysis. I am passionate about learning and applying new concepts to my work, and enjoy facing a challenge head on. One of my career goals is to become a better developer through constant learning and experimentation; a goal that I also apply to life in general.</p>
 		</div>
 	</div>
-	<div class="row about-interests">
+	<div class="row about-subsection about-interests">
 		<div class="column">
 			<h2>Interests</h2>
 			<p>Aside from my inner "geek zone", my interests also include relaxation through reading and choral music.</p>
 		</div>
 	</div>
-	<div class="row about-objective">
+	<div class="row about-subsection about-objective">
 		<div class="column">
 			<h2>Objective</h2>
 			<p>To obtain a coop position in a environment where my passion for development and learning can be used to solve business problems effectively.</p>
 		</div>
 	</div>
-	<div class="row about-education">
+	<div class="row about-subsection about-education">
 		<div class="column">
 			<h2>Education</h2>
-			<h3>Computer Programmer/Analyst<span class="date-right">2014&mdash;present</span></h3>
+			<h3>
+				<a href="//www.conestogac.on.ca/fulltime/computer-programmer-analyst-optional-co-op" target="_blank">Computer Programmer/Analyst</a>
+				<span class="date-right">2014&mdash;present</span>
+			</h3>
 			<p>Conestoga College, Kitchener, ON</p>
 			<ul>
 				<li>Expected graduation date: April 2018</li>
@@ -47,11 +50,14 @@ require_once(HEADER_FRAGMENT);
 			</ul>
 		</div>
 	</div>
-	<div class="row about-employment">
+	<div class="row about-subsection about-employment">
 		<div class="column">
 			<h2>Employment</h2>
 
-			<h3>Web Developer <span class="employment-type">(Coop)</span></h3>
+			<h3>
+				<a href="//www.netsweeper.com/">Web Developer</a>
+				<span class="employment-type">(Coop)</span>
+			</h3>
 			<p>Netsweeper Inc, Kitchener, ON<span class="date-right">2016&mdash;Present</span></p>
 			<ul>
 				<li>Analyze and apply fixes to bugs while working in a sprint-based development cycle under source control.</li>
@@ -59,7 +65,10 @@ require_once(HEADER_FRAGMENT);
 				<li>Assist with quality assurance testing on product involving writing unit tests and running manual tests.</li>
 			</ul>
 
-			<h3>Web Developer <span class="employment-type">(Work Study)</span></h3>
+			<h3>
+				<a href="//www.conestogac.on.ca">Web Developer</a>
+				<span class="employment-type">(Work Study)</span>
+			</h3>
 			<p>Conestoga College, Kitchener, ON<span class="date-right">2015&mdash;2016</span></p>
 			<ul>
 				<li>Responsible for creating, updating and maintaining web pages based on content provided by Project Manager and clients.</li>
