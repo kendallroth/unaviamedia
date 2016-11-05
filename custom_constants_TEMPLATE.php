2b77286c2c431db8356cf71a321309a463bac6de
UnaviaMedia <kroth@unaviamedia.ca>
2016-11-05 11:52:06 -0400

    Add RECAPTCHA_SECRET key constant to constants template

--- custom_constants_TEMPLATE.php
+++ custom_constants_TEMPLATE.php
@@ -1,8 +1,11 @@
 <?php
 
 //Mail constants
-define("MAIL_ADDRESS",	"");
-define("SMTP_HOST",		"");
-define("SMTP_PORT",		"");
-define("SMTP_USERNAME",	"");
-define("SMTP_PASSWORD",	"");
+define("MAIL_ADDRESS",		"");
+define("SMTP_HOST",			"");
+define("SMTP_PORT",			"");
+define("SMTP_USERNAME",		"");
+define("SMTP_PASSWORD",		"");
+
+//ReCAPTCHA constants
+define("RECAPCHA_SECRET", 	"");
