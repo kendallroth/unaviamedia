3e1d01dfb5ad4bb5c1c34aa797197e2d8f81d1de
UnaviaMedia <kroth@unaviamedia.ca>
2016-11-06 00:10:55 -0400

    Update timer styles

--- unavia/controllers/contact_controller.php
+++ unavia/controllers/contact_controller.php
@@ -101,7 +101,8 @@ if (isset($_POST["contactSubmit"])) {
 
 	//Create the form message bar to indicate status of form submission
 	$mailMessage = new Message("contact-message", $message, $messageType);
-	$mailMessage->timeout = 5;
+	$mailMessage->timeout = 10;
+	$mailMessage->timerSize = 20;
 	$messageHTML = $mailMessage->create();
 }
 
