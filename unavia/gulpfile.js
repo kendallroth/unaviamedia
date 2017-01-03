f95d8ef0cdd404fdff417888391a5b1c1689e20b
UnaviaMedia <kroth@unaviamedia.ca>
2017-01-02 19:36:02 -0500

    Update gulpfile

--- unavia/gulpfile.js
+++ unavia/gulpfile.js
@@ -1,29 +1,24 @@
 var gulp	= require("gulp");
-var del		= require("del");
+var gutil	= require("gulp-util");
 var $    	= require("gulp-load-plugins")();
 
 gulp.task("sass", function() {
-	//Delete old files
-	//del.sync('dist/css');
-
-	//setTimeout(function() {
-		return gulp.src("src/scss/app.scss")
-			//Compile the SCSS to CSS
-			.pipe($.sass({
-				includePaths: [
-					"bower_components/foundation-sites/scss",
-					"bower_components/motion_ui/src",
-					"bower_components/foundation-icon-fonts"
-				]
-			}))
-			//Add vendor prefixes to CSS
-			.pipe($.autoprefixer({
-				browsers: ["last 2 version"],
-				cascade: false
-			}))
-			//Output the CSS
-			.pipe(gulp.dest("dist/css"));
-	//}, 10);
+	return gulp.src("src/scss/app.scss")
+		//Compile the SCSS to CSS
+		.pipe($.sass({
+			includePaths: [
+				"bower_components/foundation-sites/scss",
+				"bower_components/motion_ui/src",
+				"bower_components/foundation-icon-fonts"
+			]
+		}))
+		//Add vendor prefixes to CSS
+		.pipe($.autoprefixer({
+			browsers: ["last 2 version"],
+			cascade: false
+		}))
+		//Output the CSS
+		.pipe(gulp.dest("dist/css"));
 });
 
 //Copy fonts to the output directory
@@ -33,7 +28,7 @@ gulp.task("fonts", function() {
 		.pipe(gulp.dest("dist/css"));
 });
 
-//TODO: Add script minification
+//TODO: Add script concatenation and minification
 gulp.task("scripts", function() {
 	return gulp.src(["src/js/app.js", "src/js/utilities.js"])
 		//Output the JS
@@ -41,11 +36,16 @@ gulp.task("scripts", function() {
 });
 
 
+//Gulp build task
+gulp.task("build", ["sass", "scripts", "fonts"], function() {
+	gutil.log("Building...");
+});
+
 //Gulp watch task
 gulp.task("watch", ["sass", "scripts", "fonts"], function() {
-	gulp.watch("src/scss/*.scss", ["sass"]);
-	gulp.watch("src/js/*.js", ["scripts"]);
+	gulp.watch("src/scss/**/*.scss", ["sass"]);
+	gulp.watch("src/js/**/*.js", ["scripts"]);
 });
 
 //Default gulp task
-gulp.task("default", ["watch"]);
+gulp.task("default", ["build"]);
