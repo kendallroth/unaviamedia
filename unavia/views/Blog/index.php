29783f9e529289ef924a24361bc76a4740aff815
UnaviaMedia <kroth@unaviamedia.ca>
2017-01-04 21:45:41 -0500

    Add initial index Blog view for #39

---
+++ unavia/views/Blog/index.php
@@ -0,0 +1,35 @@
+<?php
+$PAGE_TITLE = "Blog";
+require_once(FRAGMENT_HEADER);
+?>
+
+<section class="page__wrapper">
+	<div class="row">
+		<div class="column text-center">
+			<h1>Blog</h1>
+		</div>
+	</div>
+</section>
+
+<section class="row align-center">
+	<div class="column medium-10 large-8">
+		<table>
+			<thead>
+				<th>Title</th>
+				<th>Description</th>
+			</thead>
+			<tbody>
+				<?php foreach($posts as $post) { ?>
+				<tr>
+					<td><?=$post->title ?></td>
+					<td><?=$post->description ?></td>
+				</tr>
+				<?php } ?>
+			</tbody>
+		</table>
+	</div>
+</section>
+
+<?php
+require_once(FRAGMENT_FOOTER);
+?>
