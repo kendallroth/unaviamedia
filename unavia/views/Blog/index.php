ce0379d97fa97ec657253a8818aeefb13c85b2d7
UnaviaMedia <kroth@unaviamedia.ca>
2017-01-04 22:56:39 -0500

    Update blog index page for #39

--- unavia/views/Blog/index.php
+++ unavia/views/Blog/index.php
@@ -12,11 +12,28 @@ require_once(FRAGMENT_HEADER);
 </section>
 
 <section class="row align-center">
-	<div class="column medium-10 large-8">
+	<div class="column small-12 medium-9 large-8">
+		<?php foreach($posts as $post) { ?>
+			<div class="post__card">
+				<div class="card__header">
+					<span class="card__title">
+						<a href="<?=URL_BLOG?>/<?=$post->id ?>/<?=$post->title ?>"><?=$post->title ?></a>
+					</span>
+					<span class="card__info clearfix">
+						<?=$post->author ?> | <?=date_format(date_create($post->dateCreated), "Y-M-d") ?>
+					</span>
+				</div>
+				<div class="card__body">
+					<?=$post->description ?>
+				</div>
+			</div>
+		<?php } ?>
+	</div>
+	<!-- TODO: Create blog archive tool -->
+	<div class="column small-12 medium-2 large-3">
 		<table>
 			<thead>
 				<th>Title</th>
-				<th>Description</th>
 			</thead>
 			<tbody>
 				<?php foreach($posts as $post) { ?>
@@ -24,7 +41,6 @@ require_once(FRAGMENT_HEADER);
 					<td>
 						<a href="<?=URL_BLOG?>/<?=$post->id ?>/<?=$post->title ?>"><?=$post->title ?></a>
 					</td>
-					<td><?=$post->description ?></td>
 				</tr>
 				<?php } ?>
 			</tbody>
